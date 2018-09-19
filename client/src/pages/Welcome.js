21c0a8534198e4f132510c30b354369fc48c6189
Jennifer Cobb <jenn.cobb.m@gmail.com>
2018-09-19 17:37:37 -0400

    styling

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,12 +1,34 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+import NumCard from "../components/Card"
 
 const Welcome = () => {
-    return(
-     <div>
-      is the Welcome Page
-     </div>
-    )
+  return (
+    <div>
+      <Hero backgroundImage="assets/images/plane_cropped.jpg">
+        <h1>Globe Trotters</h1>
+
+      </Hero>
+      <Container style={{ marginTop: 50 }}>
+        <Row>
+          <Col size="md-12">
+            <h1>Welcome, Bob!</h1>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col size="md-6">
+            <NumCard />
+          </Col>
+        </Row>
+
+      </Container>
+    </div>
+  )
 
 };
- 
- export default Welcome;
+
+export default Welcome;
