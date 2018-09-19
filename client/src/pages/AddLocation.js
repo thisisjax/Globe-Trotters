58160fd27d72cc041fa52d03aac2b82037b32b7a
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-09-19 13:32:10 -0400

    routing to all pages done

---
+++ client/src/pages/AddLocation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AddLocation = () => {
+    return(
+     <div>
+     
+      <h1> is the AddLocation Page</h1>
+     </div>
+     
+    )
+};
+ 
+ export default AddLocation;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Login = () => {
+    return(
+    <div>
+     <h1> is the Login Page</h1>
+    </div>
+   )
+};
+
+export default Login;
